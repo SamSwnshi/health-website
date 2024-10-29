4d78d19db2d270da6e9eb231cca8b53f6db6babc
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-10-29 15:10:30 +0530

    footer completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,36 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaSquareInstagram } from "react-icons/fa6";
-import { FaFacebookSquare, FaYoutube, FaGithub } from "react-icons/fa";
+import { FaFacebookSquare, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="shadow-xl bg-[#1A3636] text-white">
       <footer>
-        <div>
-          <div>
-            <h1 className="text-lg  sm:text-xl font-bold cursor-pointer mb-4">
+        <div className="flex flex-col md:flex-row py-4">
+          <div className="w-full md:w-1/3 text-center mb-4 md:mb-0">
+            <h1 className="text-lg sm:text-xl font-bold cursor-pointer mb-4">
               Hospital Management
             </h1>
-            <p>
+            <p className="px-4 md:px-0">
               Our hospital management system streamlines medical processes,
               enhances patient care, and supports healthcare professionals in
               delivering efficient, high-quality service.
             </p>
           </div>
-          <div>
-            <div>
-              <h4 className="text-lg sm:text-xl font-bold cursor-pointer mb-4">
-                Quick Links
-              </h4>
-            </div>
-
-            <div className="flex">
+          <div className="w-full md:w-1/3 text-center mb-4 md:mb-0">
+            <h4 className="text-lg sm:text-xl font-bold cursor-pointer mb-4">
+              Quick Links
+            </h4>
+            <div className="flex flex-wrap justify-center gap-4 md:gap-7">
               <div>
                 <p>Home</p>
                 <Link
                   to="/"
-                  className="text-gray-200 hover:text-black  sm:text-base"
+                  className="text-gray-200 hover:text-black sm:text-base"
                   style={{ fontSize: "15px" }}
                 />
               </div>
@@ -38,7 +35,7 @@ const Footer = () => {
                 <p>About</p>
                 <Link
                   to="/about"
-                  className="text-gray-200 hover:text-black  sm:text-base"
+                  className="text-gray-200 hover:text-black sm:text-base"
                   style={{ fontSize: "15px" }}
                 />
               </div>
@@ -46,7 +43,7 @@ const Footer = () => {
                 <p>Contact</p>
                 <Link
                   to="/contact"
-                  className="text-gray-200 hover:text-black  sm:text-base"
+                  className="text-gray-200 hover:text-black sm:text-base"
                   style={{ fontSize: "15px" }}
                 />
               </div>
@@ -54,29 +51,35 @@ const Footer = () => {
                 <p>Privacy And Policy</p>
                 <Link
                   to="privacyandpolicy"
-                  className="text-gray-200 hover:text-black  sm:text-base"
+                  className="text-gray-200 hover:text-black sm:text-base"
                   style={{ fontSize: "15px" }}
                 />
               </div>
             </div>
           </div>
-          <div>
+          <div className="w-full md:w-1/3 text-center">
             <h4 className="text-lg sm:text-xl font-bold cursor-pointer mb-4">
               Contact
             </h4>
             <div className="flex justify-center gap-4 mb-2">
-              <FaFacebookSquare className="text-2xl cursor-pointer hover:text-gray-300" />
-              <FaYoutube className="text-2xl cursor-pointer hover:text-gray-300" />
-              <FaSquareInstagram className="text-2xl cursor-pointer hover:text-gray-300" />
-              <FaGithub className="text-2xl cursor-pointer hover:text-gray-300" />
+              <a href="https://www.facebook.com/sameer.suryawanshi1" target="_blank" rel="noopener noreferrer">
+                <FaFacebookSquare className="text-2xl cursor-pointer hover:text-gray-300" />
+              </a>
+              <a href="https://www.linkedin.com/in/sameer-suryawanshi/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin className="text-2xl cursor-pointer hover:text-gray-300" />
+              </a>
+              <a href="https://www.instagram.com/_sameer_suryawanshi_/" target="_blank" rel="noopener noreferrer">
+                <FaSquareInstagram className="text-2xl cursor-pointer hover:text-gray-300" />
+              </a>
+              <a href="https://github.com/SamSwnshi" target="_blank" rel="noopener noreferrer">
+                <FaGithub className="text-2xl cursor-pointer hover:text-gray-300" />
+              </a>
             </div>
-            <div className="flex items-center justify-evenly gap-2 mb-1 text-sm sm:text-base text-center">
-              <p>hotelmanagement@gmail.com</p>
-            </div>
+            {/* <p>hospitalmanagement@gmail.com</p> */}
           </div>
         </div>
       </footer>
-      <p className="flex justify-center border-t border-gray-700 py-2 text-sm">
+      <p className="flex justify-center border-t border-gray-700 p-2 text-sm">
         &copy; CopyRight By Sameer
       </p>
     </div>
